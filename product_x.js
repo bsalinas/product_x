3f61bc46e60aa08a16dec6445c9e46a74b42ee8f
Ben Salinas <ben@luminairecoffee.com>
2021-08-10 14:38:37 -0500

    Revert to an older version

--- product_x.js
+++ product_x.js
@@ -1,21 +1,10 @@
 const ALERT_SERVICE = "00000000-1212-efde-1523-780f0000000d"
-const CURRENT_READING_CHAR = "00000001-1212-efde-1523-780f0000000d"
-const CURRENT_REALERT_CHAR = "00000002-1212-efde-1523-780f0000000d"
+const VOLTAGE_YELLOW_CHAR = "00000007-1212-efde-1523-780f0000000d"
+const VOLTAGE_RED_CHAR = "00000008-1212-efde-1523-780f0000000d"
 const CURRENT_YELLOW_CHAR = "00000003-1212-efde-1523-780f0000000d"
 const CURRENT_RED_CHAR = "00000004-1212-efde-1523-780f0000000d"
 const VOLTAGE_READING_CHAR = "00000005-1212-efde-1523-780f0000000d"
-const VOLTAGE_REALERT_CHAR = "00000006-1212-efde-1523-780f0000000d" 
-const VOLTAGE_YELLOW_CHAR = "00000007-1212-efde-1523-780f0000000d"
-const VOLTAGE_RED_CHAR = "00000008-1212-efde-1523-780f0000000d"
-
-const ALGORITHM_CONFIG = "00000009-1212-efde-1523-780f0000000d"
-
-const UI_SERVICE = "00000000-1212-efde-1523-780d000000f0"
-const UI_BRIGHTNESS_CHAR = "00000001-1212-efde-1523-780d000000f0"
-
-
-const DEVICE_INFORMATION_SERVICE = "0000180a-0000-1000-8000-00805f9b34fb"
-const FW_VERSION = "00002a26-0000-1000-8000-00805f9b34fb"
+const  CURRENT_READING_CHAR = "00000001-1212-efde-1523-780f0000000d"
 class ProductX {
 
   constructor(disconnect_cb) {
@@ -35,7 +24,7 @@ class ProductX {
     .then(device => {
       this.device = device;
       this.device.addEventListener('gattserverdisconnected', this.onDisconnected);
-    })
+    });
   }
   
   connect() {
@@ -81,25 +70,6 @@ class ProductX {
     .then(service => service.getCharacteristic(VOLTAGE_RED_CHAR))
     .then(characteristic => characteristic.writeValue(data));
   }
-  readVoltageRealert() {
-    return this.device.gatt.getPrimaryService(ALERT_SERVICE)
-    .then(service => service.getCharacteristic(VOLTAGE_REALERT_CHAR))
-    .then(characteristic => characteristic.readValue())
-    .then(value => {
-        console.log(value);
-        let view = new Uint32Array(value.buffer);
-        console.log(view[0]);
-        return view[0];
-    })
-  }
-
-  writeVoltageRealert(value) {
-    let data = new Uint32Array(1);
-    data[0] = value;
-    return this.device.gatt.getPrimaryService(ALERT_SERVICE)
-    .then(service => service.getCharacteristic(VOLTAGE_REALERT_CHAR))
-    .then(characteristic => characteristic.writeValue(data));
-  }
 
    readCurrentYellow() {
     return this.device.gatt.getPrimaryService(ALERT_SERVICE)
@@ -137,24 +107,6 @@ class ProductX {
     .then(characteristic => characteristic.writeValue(data));
   }
 
-  readCurrentRealert() {
-    return this.device.gatt.getPrimaryService(ALERT_SERVICE)
-    .then(service => service.getCharacteristic(CURRENT_REALERT_CHAR))
-    .then(characteristic => characteristic.readValue())
-    .then(value => {
-        let view = new Uint32Array(value.buffer);
-        return view[0];
-    })
-  }
-
-  writeCurrentRealert(value) {
-    let data = new Uint32Array(1);
-    data[0] = value;
-    return this.device.gatt.getPrimaryService(ALERT_SERVICE)
-    .then(service => service.getCharacteristic(CURRENT_REALERT_CHAR))
-    .then(characteristic => characteristic.writeValue(data));
-  }
-
   readCurrentField() {
     return this.device.gatt.getPrimaryService(ALERT_SERVICE)
     .then(service => service.getCharacteristic(CURRENT_READING_CHAR))
@@ -174,74 +126,6 @@ class ProductX {
     })
   }
 
-  readBrightness()
-  {
-    return this.device.gatt.getPrimaryService(UI_SERVICE)
-    .then(service => service.getCharacteristic(UI_BRIGHTNESS_CHAR))
-    .then(characteristic => characteristic.readValue())
-    .then(value => {
-        let view = new Uint8Array(value.buffer);
-        return view[0];
-    })
-  }
-  writeBrightness(value)
-  {
-    if(value > 255) value = 255;
-    let data = new Uint8Array(1);
-    data[0] = value;
-    return this.device.gatt.getPrimaryService(UI_SERVICE)
-    .then(service => service.getCharacteristic(UI_BRIGHTNESS_CHAR))
-    .then(characteristic => characteristic.writeValue(data));
-  }
-  
-  readAlgorithmConfig() {
-    return this.device.gatt.getPrimaryService(ALERT_SERVICE)
-    .then(service=> service.getCharacteristics())
-    .then(all_characteristics => {
-        for(let idx in all_characteristics)
-        {
-            let ch = all_characteristics[idx];
-            if(ch.uuid == ALGORITHM_CONFIG)
-            {
-                return ch.readValue();
-            }
-        }
-        return {"buffer":[0]}
-    })
-    // .then(service => service.getCharacteristic(ALGORITHM_CONFIG))
-    // .then(characteristic => characteristic.readValue())
-    // .error(error => {
-    //     return 0;
-    // })
-    .then(value => {
-        let view = new Uint32Array(value.buffer);
-        return view[0];
-    })
-
-  }
-
-  writeAlgorithmConfig(value) {
-    let data = new Uint32Array(1);
-    data[0] = value;
-    return this.device.gatt.getPrimaryService(ALERT_SERVICE)
-    .then(service => service.getCharacteristic(ALGORITHM_CONFIG))
-    .then(characteristic => characteristic.writeValue(data));
-  }
-
-  readFirmwareVersion(){
-    return this.device.gatt.getPrimaryService(DEVICE_INFORMATION_SERVICE)
-    .then(service => service.getCharacteristic(FW_VERSION))
-    .then(characteristic => characteristic.readValue())
-    .then(value => {
-        console.log(value)
-        let enc = new TextDecoder("utf-8");
-        let arr = new Uint8Array(value.buffer);
-        let version = enc.decode(arr);
-        console.log(version)
-        return version;
-    })
-  }
-
   disconnect() {
     if (!this.device) {
       return Promise.reject('Device is not connected.');
@@ -257,4 +141,3 @@ class ProductX {
     }
   }
 }
-
